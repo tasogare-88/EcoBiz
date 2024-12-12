40e90797e6a1c7f190831dad1060915c6e80d9f3
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2024-12-12 16:53:14 +0900

    design: 💄 アプリ右上のDEBUGの文言削除

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'EcoBiz',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
