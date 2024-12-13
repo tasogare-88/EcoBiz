f4b5b7f952d4c3abe71381efcd88e850a3654d47
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2024-12-13 15:48:42 +0900

    design: 💄 デザイン

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,27 +1,20 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../features/steps/presentation/steps_screen.dart';
 import 'home_view_model.dart';
 
-class HomeScreen extends ConsumerStatefulWidget {
+class HomeScreen extends StatefulHookConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+  HomeScreenState createState() => HomeScreenState();
 }
 
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // initStateで歩数データを取得
-    Future.microtask(
-        () => ref.read(homeViewModelProvider.notifier).fetchSteps());
-  }
-
+class HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final homeState = ref.watch(homeViewModelProvider);
@@ -51,23 +44,23 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            _buildHeader(homeState),
-            _buildCarousel(),
-            _buildMenuGrid(),
+            buildHeader(homeState),
+            buildCarousel(),
+            buildMenuGrid(),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildHeader(HomeState state) {
+  Widget buildHeader(HomeState state) {
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
         children: [
           CircleAvatar(
             radius: 24,
-            backgroundColor: const Color.fromARGB(255, 234, 221, 255),
+            backgroundColor: const Color(0xFFEADDFF),
             child: Icon(Icons.person, size: 30),
           ),
           const SizedBox(width: 16),
@@ -99,42 +92,112 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     );
   }
 
-  Widget _buildCarousel() {
-    return Container(
-      height: 200,
-      child: PageView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 8),
-            child: Center(
-              child: Text('カルーセル ${index + 1}'),
+  Widget buildCarousel() {
+    return HookBuilder(
+      builder: (context) {
+        final pageController = usePageController();
+        final currentPage = useState(0);
+
+        useEffect(() {
+          final timer = Timer.periodic(Duration(seconds: 5), (timer) {
+            if (pageController.hasClients) {
+              final nextPage = (currentPage.value + 1) % 5;
+              pageController.animateToPage(
+                nextPage,
+                duration: Duration(milliseconds: 500), // 5秒で次のカルーセルに移動
+                curve: Curves.easeInOut,
+              );
+            }
+          });
+
+          return timer.cancel;
+        }, []);
+
+        return Stack(
+          children: [
+            Container(
+              height: 200,
+              child: PageView.builder(
+                controller: pageController,
+                itemCount: 5,
+                onPageChanged: (index) {
+                  currentPage.value = index;
+                },
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 8),
+                    child: Center(
+                      child: Text('カルーセル ${index + 1}'),
+                    ),
+                  );
+                },
+              ),
             ),
-          );
-        },
-      ),
+            // 左矢印
+            Positioned(
+              left: 8,
+              top: 0,
+              bottom: 0,
+              child: Center(
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: () {
+                    pageController.previousPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                ),
+              ),
+            ),
+            // 右矢印
+            Positioned(
+              right: 8,
+              top: 0,
+              bottom: 0,
+              child: Center(
+                child: IconButton(
+                  icon: Icon(Icons.arrow_forward_ios),
+                  onPressed: () {
+                    pageController.nextPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                ),
+              ),
+            ),
+            // ページインジケーター
+            Positioned(
+              bottom: 16,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(5, (index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4),
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: currentPage.value == index
+                          ? Colors.blue
+                          : Colors.grey.withOpacity(0.5),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 
-  Widget _buildMenuGrid() {
-    final menuItems = [
-      MenuItemData('記録', Icons.book),
-      MenuItemData('ランキング', Icons.leaderboard),
-      MenuItemData('ショップ', Icons.store),
-      MenuItemData('タスク', Icons.checklist),
-    ];
-
-    return GridView.count(
-      shrinkWrap: true,
-      crossAxisCount: 2,
-      padding: EdgeInsets.all(16),
-      children: menuItems.map((item) => _buildMenuItem(item)).toList(),
-    );
-  }
-
-  Widget _buildMenuItem(MenuItemData item) {
+  Widget buildMenuItem(MenuItemData item) {
     return Card(
-      color: Color(0xFFFFE4C4),
+      color: Color(0xFFFFDEAA),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -149,6 +212,24 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  Widget buildMenuGrid() {
+    final menuItems = [
+      MenuItemData('記録', Icons.book),
+      MenuItemData('ランキング', Icons.leaderboard),
+      MenuItemData('ショップ', Icons.store),
+      MenuItemData('タスク', Icons.checklist),
+    ];
+
+    return GridView.count(
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      padding: EdgeInsets.all(16),
+      mainAxisSpacing: 16,
+      crossAxisSpacing: 16,
+      children: menuItems.map((item) => buildMenuItem(item)).toList(),
     );
   }
 }
