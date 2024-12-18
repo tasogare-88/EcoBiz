716fca4a4a46510b2650d15cbbff06148677ccb3
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2024-12-18 21:01:07 +0900

    feat: ✨ 歩数対戦ロジックと画面

---
+++ lib/features/communication/data/ble_repository.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ble_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bleRepositoryHash() => r'e37d445480cec61fe15788c38ed636804725a0a3';
+
+/// See also [BleRepository].
+@ProviderFor(BleRepository)
+final bleRepositoryProvider =
+    AutoDisposeNotifierProvider<BleRepository, List<BattleDevice>>.internal(
+  BleRepository.new,
+  name: r'bleRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bleRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BleRepository = AutoDisposeNotifier<List<BattleDevice>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/features/communication/presentation/communication_screen.dart
+++ lib/features/communication/presentation/communication_screen.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CommunicationScreen extends StatelessWidget {
+import './communication_view_model.dart';
+
+class CommunicationScreen extends ConsumerWidget {
   const CommunicationScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('通信画面（未実装）'),
-    ));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(communicationViewModelProvider);
+
+    // if (!state.isBluetoothAvailable) {
+    //   return const Scaffold(
+    //     body: Center(
+    //       child: Text('Bluetoothを有効にしてください'),
+    //     ),
+    //   );
+    // }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('対戦相手'),
+        backgroundColor: const Color(0xFFB5D4E4),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () =>
+            ref.read(communicationViewModelProvider.notifier).refreshDevices(),
+        child: state.devices.isEmpty
+            ? const Center(
+                child: Text('近くに対戦相手が見つかりません'),
+              )
+            : ListView.builder(
+                itemCount: state.devices.length,
+                itemBuilder: (context, index) {
+                  final device = state.devices[index];
+                  return Card(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    color: const Color(0xFFFFE4C4),
+                    child: ListTile(
+                      leading: const CircleAvatar(
+                        backgroundColor: Color(0xFFEADDFF),
+                        child: Icon(Icons.person),
+                      ),
+                      title: Text(device.name),
+                      onTap: () {
+                        // TODO: 対戦処理の実装
+                      },
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
   }
 }
