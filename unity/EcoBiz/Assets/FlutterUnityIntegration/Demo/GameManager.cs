345424884ddf7827e1e23240a5a5c1293dfc908f
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2024-12-20 16:26:43 +0900

    feat: ✨ Unity連携

--- unity/EcoBiz/Assets/FlutterUnityIntegration/Demo/GameManager.cs
+++ unity/EcoBiz/Assets/FlutterUnityIntegration/Demo/GameManager.cs
@@ -35,4 +35,9 @@ public class GameManager : MonoBehaviour
                 break;
         }
     }
+
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
 }
